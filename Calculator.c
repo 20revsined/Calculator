b16df329dc537658155d129c78d8108ec60cdb64
20revsined <44682153+20revsined@users.noreply.github.com>
2019-03-11 09:05:01 -0400

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,23 @@
+int main() {
+printf(add(5, 6));
+return 0;
+}
+
+double add(double x, double y) {
+  return (x + y);
+}
+
+double subtract(double x, double y)
+{
+  return (x - y);
+}
+
+double multiply(double x, double y)
+{
+  return (x * y);
+}
+
+double divide(double x, double y)
+{
+  return (x / y);
+}
